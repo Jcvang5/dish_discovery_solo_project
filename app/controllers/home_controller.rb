58b5424b76fc0e9cc37821e9977fa08ab6b80bd6
Jason Vang <jvang@Jasons-MacBook-Air.local>
2023-12-21 10:23:08 -0600

    Changed configs for controller and database.yml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,15 @@
 
 class HomeController < ApplicationController
   before_action :set_gon_variables
-  def index; end
+  def index; 
 
+  end
+
+  private
   def set_gon_variables
     gon.env_variables = {
-      SPOONACULAR_API_KEY: ENV['SPOONACULAR_API_KEY']
+      SPOONACULAR_API_KEY: ENV['SPOONACULAR_API_KEY'],
+      MYAPP_DATABASE_PASSWORD: ENV['MYAPP_DATABASE_PASSWORD']
     }
   end
 end
